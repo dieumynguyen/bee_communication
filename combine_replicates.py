1dbe0fe4bf11d05bfe0c95016e07d9a2ef0f78c8
dieumynguyen <ding1018@colorado.edu>
2018-05-25 14:09:45 -0700

    combine_replicates.py

--- combine_replicates.py
+++ combine_replicates.py
@@ -54,13 +54,63 @@ def load_config_measurements(experiment_dir, replicate_i):
 
 ########################################################################
 
+def combine_replicates(data_folders, param_set):
 
+    ''' This function finds replicates of a set of parameters across
+    all experiment runs. Iteratively calls the function
+    "load_config_measurements" to create a dictionary for a single
+    replicate. Then combine all replicates' data into a list, and write
+    to a JSON file for further data analysis. Will run this 256 times in
+    main() for 256 sets.
+    '''
 
+    # List to contain all replicates
+    all_replicates = []
 
-def main():
-    # Test "load_config_measurements" function
-    experiment_dir = "experiments/05M_24D-13H_48M_06S/experiment0_Q0.01_W0.005_D0.05_T0.001"
-    d = load_config_measurements(experiment_dir, 1)
-    d["Replicate 1"]['queen_bee_concentration']
+    # If find the param_set in a data_folders[i], get the index of
+    # that data_folders[i]
+    data_folder_i = []
+    for i in range(len(data_folders)):
+        found_index = data_folders[i].find(param_set)
+        if found_index != -1:
+            # print(i)
+            data_folder_i.append(i)
+
+    # Load data using "load_config_measurements"
+    for j in range(len(data_folder_i)):
+        data = load_config_measurements(data_folders[data_folder_i[j]], j+1)
+        all_replicates.append(data)
+
+    # Truncate string "param_set" to save as name for json file
+    truncate_name = param_set[12:]
+
+    # Write the cumulative list containing all replicates to JSON
+    with open('combined_replicates/{}.json'.format(truncate_name), 'w') as outfile:
+        json.dump(all_replicates, outfile)
 
     return None
+
+################################## MAIN #################################
+def main():
+    # Test "load_config_measurements" function
+    # experiment_dir = "experiments/05M_24D-16H_04M_42S/experiment0_Q0.01_W0.005_D0.05_T0.001"
+    # d = load_config_measurements(experiment_dir, 1)
+    # print(d["Replicate 1"]['worker_bee_concentration'])
+
+    # All data folders: ultimately 2560
+    data_folders = glob2.glob("experiments/*/*")
+    # print(data_folders)
+
+    # List of 256 unique sets of parameters
+    # Any of the 10 folders will contain 256 sets with same names, so choose any
+    sets_list = next(os.walk('experiments/05M_24D-13H_48M_06S/'))[1]
+    # print(sets_list)
+
+    # Test "combine_replicates" function using 2 sample lists above
+    for k in sets_list:
+        save_lists_json = combine_replicates(data_folders, k)
+        print("Finished combining all replicates of set: {}".format(k))
+
+    return None
+
+main()
