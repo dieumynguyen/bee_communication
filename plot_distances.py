939e822c0cc61658cded13bf885e4e8876b84dd4
dieumynguyen <ding1018@colorado.edu>
2018-05-26 17:31:05 -0700

    get_avg_distance_data

--- plot_distances.py
+++ plot_distances.py
@@ -1,3 +1,4 @@
+# %load plot_distances.py
 # Imports
 import json
 import numpy as np
@@ -11,6 +12,7 @@ import plotly.graph_objs as go
 import os
 import glob2
 import re
+# from wand.image import Image as WImage
 
 
 ############################# STYLESHEET ###############################
@@ -24,7 +26,7 @@ plt.rcParams['axes.labelsize'] = 14
 plt.rcParams['axes.labelweight'] = 'normal'
 plt.rcParams['xtick.labelsize'] = 14
 plt.rcParams['ytick.labelsize'] = 14
-plt.rcParams['legend.fontsize'] = 14
+plt.rcParams['legend.fontsize'] = 12
 plt.rcParams['figure.titlesize'] = 14
 plt.rcParams['lines.linewidth'] = 3
 params = {"ytick.color" : "#535956",
@@ -33,6 +35,37 @@ params = {"ytick.color" : "#535956",
           "axes.edgecolor" : "#535956",
           "text.color": "#535956"}
 plt.rcParams.update(params)
+
+########################################################################
+
+def standardize_filenames(fname, start_char):
+    fig_name = fname[20:fname.index(start_char)-1]
+    split_list = fig_name.split("_")
+    # print(split_list)
+
+    new_name = ""
+    for i in range(len(split_list)):
+        if len(split_list[i][1:]) > 5:
+            if int(split_list[i][6]) <= 5:
+                round_down = split_list[i][1:6]
+                new_name += split_list[i][0] + round_down + "_"
+                # print(round_down)
+            else:
+                round_up = str(float(split_list[i][1:6]) + 0.001)
+                new_name += split_list[i][0] + round_up + "_"
+                # print(round_up)
+
+        else:
+            new_name += split_list[i] + "_"
+
+    # print(new_name)
+
+    if new_name[-1] == "_":
+        new_name = new_name[:-1]
+
+    # print(new_name)
+
+    return new_name
 
 ########################################################################
 
@@ -45,13 +78,15 @@ def plot_avg_distances(data_json):
     with open("avg_distance_data/" + data_json, "r") as f:
         data = json.load(f)
 
-    fig = plt.figure(figsize=(6,4))
+    fig = plt.figure()
     ax = plt.axes()
 
+    # Standardize file names
+    fname = f.name
     start_char = "j"
-    filename = f.name
-    fig_name = filename[18:filename.index(start_char)]
+    fig_name = standardize_filenames(fname, start_char)
 
+    # Plotting
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
 
@@ -61,25 +96,26 @@ def plot_avg_distances(data_json):
         plt.xlim(0, 160)
         plt.ylim(0, 5)
 
-        plt.legend()
+        plt.legend(bbox_to_anchor=(1,0.5), loc="center left")
 
         plt.xlabel('Time (step)')
         plt.ylabel('Average distance to queen')
-        plt.title('{}'.format(fig_name[:-1]))
+        plt.title('{}'.format(fig_name))
 
         plt.tight_layout()
-        plt.savefig("figures/distance_to_queen/{}pdf".format(fig_name), transparent=True)
+        plt.savefig("figures/distance_to_queen/{}.pdf".format(fig_name), transparent=True)
 
     plt.close()
 
 ########################################################################
 
 def main():
-    # Iterate through all (256) JSON's and produce 1 figure each
-    reps_list = list(map(lambda x : x.split("/")[-1], glob2.glob("combined_replicates/*")))
 
-    for r in reps_list:
-        print("Plotting average distance to queen for: {}".format(r))
+    # Iterate through all (256) JSON's and produce 1 figure each
+    reps_list = list(map(lambda x : x.split("/")[-1], glob2.glob("avg_distance_data/Q0.5_W0.005_D0.5_T0.001.json")))
+
+    for i, r in enumerate(reps_list):
+        print("{}. Plotting average distance to queen for: {}".format(i+1, r))
         plot_avg_distances(r)
 
 main()
