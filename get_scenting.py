4bf52cb815525b595675d83d5d2a355bf31dc4d1
dieumynguyen <ding1018@colorado.edu>
2018-06-05 12:52:29 -0700

    get_scenting.py

---
+++ get_scenting.py
@@ -0,0 +1,50 @@
+# Imports
+import json
+import numpy as np
+import os
+import glob2
+import re
+
+########################################################################
+def get_position_data(param_set_json):
+
+    ''' Load in each JSON created by combine_replicates.py and position_history data '''
+
+    with open("combined_replicates/" + param_set_json, "r") as f:
+        data = json.load(f)
+
+    sample_length = len(data[0]["Replicate {}".format(1)]["position_history"])
+
+    all_distances = {}
+    for j in range(len(data)):  # Loop over each replicate
+
+        # Loop over 50 workers
+        for i in range(1, 51):
+            position = data[j]["Replicate {}".format(j+1)]["position_history"]["worker_{}".format(i)]
+            all_distances["worker_{}".format(i)] = position
+
+
+    # Write truncated data to JSON
+    start_char = "j"
+    filename = f.name
+    name = filename[20:filename.index(start_char)]
+
+    with open('position_data/{}json'.format(name), 'w') as outfile:
+        json.dump(all_distances, outfile)
+
+    return None
+
+################################ MAIN #################################
+def main():
+    # Test on one json
+    # get_distances("Q0.01_W0.005_D0.05_T0.001.json")
+
+    # Iterate through all (256) JSON's and produce 1 file each
+    reps_list = list(map(lambda x : x.split("/")[-1], glob2.glob("combined_replicates/*")))
+
+    for r in reps_list:
+        print("Getting position data for: {}".format(r))
+        get_position_data(r)
+
+if __name__ == '__main__':
+    main()
