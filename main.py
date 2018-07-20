0a533411567fb37b10072f43234ce2d9ff67cbf8
dieumynguyen <ding1018@colorado.edu>
2018-07-19 17:18:57 -0700

    fixed worker emission

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def init_factors():
     global worker_bee_concentrations
     # worker_bee_concentrations = np.linspace(0.005, 0.4, CONDITION_COUNTS["worker_concentration"])
     # worker_bee_concentrations = [0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5]
-    worker_bee_concentrations = [0.01]
+    worker_bee_concentrations = [0.05]
 
     global diffusion_coefficients
     # diffusion_coefficients = np.linspace(0.05, 0.5, CONDITION_COUNTS["diffusion_coefficient"])
